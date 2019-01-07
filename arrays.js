e55700bc0e0dddaa67000af5900e6c7f49bad482
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-07 21:00:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
-  
+  array.push(element);
 }
-var destructivelyAddElementToBeginningOfArray
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array[0] = element;
+}
