3ad6291a278026558d2ad727aec3a9d645af72f4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-07 21:37:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,29 +13,32 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 function addElementToEndOfArray(array, element) {
   return [...array, element];
 }
+
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array
   
 }
+
 function accessElementInArray(array, index) {
   return array[index];
   
 }
+
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
   
 }
+
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
+
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
-  return array;
+  return array.pop();
 }
+
 function removeElementFromEndOfArray(array) {
-  var new_array = array;
-  new_array.slice(0, array.length()-1);
-  return new_array
+  return array.pop();
 }
