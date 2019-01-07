523cef68c97af47381c0fbf33cee7e423bee2d3e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-07 21:28:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,27 +1,34 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
 function addElementToBeginningOfArray(array, element) {
-  const new_array = array.unshift(element);
-  return new_array;
+  return [element, array];
+  
 }
+
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array[0] = element;
   return array;
+  
 }
 
 function addElementToEndOfArray(array, element) {
   var new_array = array.push(element)
   return new_array;
+  
 }
 function destructivelyAddElementToEndOfArray(array, element) {
   array[array.length()-1] = element; 
   return array;
+  
 }
 function accessElementInArray(array, index) {
   return array[index];
+  
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
+  
 }
 function removeElementFromBeginningOfArray(array) {
   array.slice(1);
